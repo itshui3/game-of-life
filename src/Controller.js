57995ed97262cd4d552c7460326dd4ba7f913ccb
Hui <jeffrey.x.hui@gmail.com>
2020-07-05 12:59:46 -0700

    wrap canvas with c ontroller, passing in a grid

---
+++ src/Controller.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import Canvas from "./Canvas.js"
+
+const Controller = (props) => {
+
+    const [grid, setGrid] = useState([
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ])
+
+    return (
+        <>
+            <Canvas grid={grid} />
+        </>
+    )
+}
+
+export default Controller
---
+++ src/grid/Row.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Row = ({row}) => {
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Row
