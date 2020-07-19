9bf0f705409d33d55e45e6bb21456edc0ca920e7
Hui <jeffrey.x.hui@gmail.com>
2020-07-19 16:35:29 -0700

    progress setTimeout written, needs debugging to make functional

--- algo.js
+++ algo.js
@@ -1,49 +1,6 @@
-function smaller(arr) {
-  
-    let smallerArr = new Array(arr.length)
-    let sortedArr = []
-    for (let i = arr.length - 1; i > -1; i--) {
-      
-      if (!sortedArr.length) {
-        sortedArr.push(arr[i])
-        smallerArr[i] = 0
-        continue
-      }
-      
-      if (smallerArr.length === 1) {
-        if (arr[i] > sortedArr[0]) {
-          smallerArr[i] = 1;
-          sortedArr.push(arr[i])
-          continue
-        }
-      }
-      
-      if (smallerArr.length > 1) {
-        let index = search(arr[i], smallerArr)
-        smallerArr[i] = index
-        sortedArr.splice(index, 0, arr[i])
-      }
-      
-    }
-    return smallerArr
-  }
-  
-  function search(target, arr) {
-  
-      let l = 0
-      let r = arr.length - 1
-      let mid;
-      
-      while (l !== r) {
-        mid = Math.floor( (r - l)/2 ) + l
-        if (target <= arr[mid]) {
-          r = mid;
-        } else if (target > arr[mid]) {
-          l = mid + 1;
-        }
-      }
-      
-      return l
-    }
 
-smaller([5, 4, 3, 2, 1])
+
+setTimeout(function reTimeout() {
+  console.log('hello world')
+
+}, 1000);
